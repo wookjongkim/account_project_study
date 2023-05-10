61b0e4d45731b5fd51aa66c3c5e4ab92ff8adbc6
wookjongkim <121083077+wookjongkim@users.noreply.github.com>
2023-05-10 16:58:07 +0900

    first commit

---
+++ src/main/java/com/example/accountproject_study/AccountProjectStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.accountproject_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountProjectStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountProjectStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/accountproject_study/AccountProjectStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.accountproject_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountProjectStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
