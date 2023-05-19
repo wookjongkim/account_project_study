84e467493849299e0aee4b2fee43b9a32cfd8906
wookjongkim <121083077+wookjongkim@users.noreply.github.com>
2023-05-19 12:26:45 +0900

    일관성 있는 예외 처리

---
+++ src/main/java/com/example/accountproject_study/dto/ErrorResponse.java
@@ -0,0 +1,14 @@
+package com.example.accountproject_study.dto;
+
+import com.example.accountproject_study.type.ErrorCode;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ErrorResponse {
+    private ErrorCode errorCode;
+    private String errorMessage;
+}
